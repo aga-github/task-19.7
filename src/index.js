51fe0032372321a4e638b7e7148564472b667a9e
Aga Matyjasik <agus.wroclaw@gmail.com>
2019-07-20 17:58:40 +0200

    Add basic files for task-19.7

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { addComment } from './actions';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+store.dispatch(addComment('drugi komentarz'));
+store.dispatch(addComment('pierwszy komentarz'));
+registerServiceWorker();
